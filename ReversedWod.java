03babd50296ca91d394b77b8ae03f5940879f452
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-07-07 08:50:48 +0530

    Update ReversedWod.java

--- ReversedWod.java
+++ ReversedWod.java
@@ -1,13 +1,27 @@
 package Mantra;
-import java.util.*;
-//Reverse Word
-public class ReversedWod {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input a word: ");
-        String input = sc.nextLine();
+import java.util.Scanner;
 
-        String reversed = new StringBuilder(input).reverse().toString();
-        System.out.println("Reverse word: " + reversed);
+public class ReversedWord {
+
+    // Entry point
+    public static void main(String[] args) {
+        String word = "shivam kumar"
+        String reversed = reverseWord(word);
+        displayReversedWord(reversed);
+    }
+
+
+    public static String reverseWord(String word) {
+        char[] characters = word.toCharArray();
+        String reversed = "";
+        for (int i = characters.length - 1; i >= 0; i--) {
+            reversed += characters[i];
+        }
+        return reversed;
+    }
+
+    // Function to display the reversed word
+    private static void displayReversedWord(String reversed) {
+        System.out.println("Reversed word: " + reversed);
     }
 }
